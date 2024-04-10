326e4982cee411fadd0b28a7e0620e7a17c9d5db
lizatamara <lizaa1609@gmail.com>
2024-04-10 18:10:59 -0400

    modifico main.py con las variables de datos.py

--- main.py
+++ main.py
@@ -4,68 +4,29 @@ import add
 import remove
 import show
 import tiempo
+import datos as d
 
-ingredientes_orden = {'masa': 'Masa Tradicional',
-                      'salsa': 'Salsa de Tomate',
-                      'ingredientes': ['Queso']
-                        }
+ingredientes_orden = d.ingredientes
 
 while True:
 
-    opcion = input('''¿Qué desea realizar?
-                 1. Cambiar tipo de Masa
-                 2. Cambiar tipo de Salsa
-                 3. Agregar Ingredientes
-                 4. Eliminar Ingredientes
-                 5. Ordenar con los Ingredientes Actuales
-                 0. Consultar ingredientes de la pizza
-                 Otro Número cancelará el pedido.
-                 > ''')
+    opcion = input(d.menu)
     
     if opcion == '1':
-        eleccion = input("""Escoja el tipo de masa:
-                         T) Tradicional
-                         D) Delgada
-                         B) Bordes de Queso
-                        > """).upper()
+        eleccion = input(d.T_MASA).upper()
         ingredientes_orden = masa.tipo_masa(ingredientes_orden, eleccion)
         
 
     elif opcion == '2':
-        eleccion = input("""Escoja el tipo de salsa:
-                         T) Salsa de Tomate
-                         A) Salsa Alfredo
-                         B) Salsa BBQ
-                         P) Salsa Pesto
-                        > """).upper()
+        eleccion = input(d.T_SALSA).upper()
         ingredientes_orden = salsa.tipo_salsa(ingredientes_orden, eleccion)
         
     elif opcion == '3':
-        eleccion = int(input("""Desea agregar un ingrediente nuevo?:
-                             1) Tomate
-                             2) Champiñones
-                             3) Aceituna
-                             4) Cebolla
-                             5) Pollo
-                             6) Jamón
-                             7) Carne
-                             8) Tocino
-                             9) Queso 
-                             > """))
+        eleccion = int(input(d.AG_INGREDIENTE))
         ingredientes_orden = add.agregar_ingrediente(ingredientes_orden, eleccion)
         
     elif opcion == '4':
-        eleccion = int(input("""Desea eliminar un ingrediente?:
-                             1) Tomate
-                             2) Champiñones
-                             3) Aceituna
-                             4) Cebolla
-                             5) Pollo
-                             6) Jamón
-                             7) Carne
-                             8) Tocino
-                             9) Queso 
-                             > """))
+        eleccion = int(input(d.QT_INGREDIENTE))
         ingredientes_orden = remove.quitar_ingredientes(ingredientes_orden, eleccion)
         
 
